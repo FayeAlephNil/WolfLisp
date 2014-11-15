938d0d78f8ae8ba68a030cfdbdff46ccd24936c8
Strikingwolf <Strikingwolf2012@gmail.com>
2014-11-15 12:54:08 -0600

    Initial Commit

---
+++ deprecated.c
@@ -0,0 +1,38 @@
+lval* eval_op(lval* x, char* op, lval* y) {
+
+    /* If either value is an error return it */
+    if (x->type == LVAL_ERR) { return x; }
+    if (y->type == LVAL_ERR) { return y; }
+
+    /* Otherwise do maths on the number values */
+    if (strcmp(op, "+") == 0) { return lval_num(x->num + y->num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x->num - y->num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x->num * y->num); }
+    if (strcmp(op, "/") == 0) {
+        /* If second operand is zero return error instead of result */
+        return y->num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x->num / y->num);
+    }
+
+    return lval_err(LERR_BAD_OP);
+}
+
+lval* eval(mpc_ast_t* t) {
+
+    if (strstr(t->tag, "number")) {
+        /* Check if there is some error in conversion */
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    }
+
+    char* op = t->children[1]->contents;
+    lval* x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
